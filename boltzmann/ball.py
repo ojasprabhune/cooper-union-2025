7474d1acfeaa929203295ecf057c3b1e4fcabfd4
ojasprabhune <prabhuneojas@gmail.com>
2025-07-15 23:49:37 -0400

    1.1

---
+++ boltzmann/ball.py
@@ -0,0 +1,10 @@
+import random
+
+class Ball:
+    def __init__(self, position, circle):
+        self.position = position 
+        self.circle = circle 
+        self.velocity_x = random.uniform(0, 0.2)
+        self.velocity_y = random.uniform(0, 0.2)
+        self.direction_x = random.choice([-1, 1])
+        self.direction_y = random.choice([-1, 1])
