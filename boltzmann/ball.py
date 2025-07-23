ad7f9783f1c05b1ed23535db134e549bfdeceb70
ojasprabhune <prabhuneojas@gmail.com>
2025-07-22 21:01:13 -0400

    1.3

--- boltzmann/ball.py
+++ boltzmann/ball.py
@@ -4,7 +4,8 @@ class Ball:
     def __init__(self, position, circle):
         self.position = position 
         self.circle = circle 
-        self.velocity_x = random.uniform(0, 0.2)
-        self.velocity_y = random.uniform(0, 0.2)
+        self.velocity_x = random.uniform(0, 0.05)
+        self.velocity_y = random.uniform(0, 0.05)
         self.direction_x = random.choice([-1, 1])
         self.direction_y = random.choice([-1, 1])
+        self.colliding = False
